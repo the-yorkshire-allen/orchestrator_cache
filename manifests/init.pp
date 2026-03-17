e4418e6d162b33834bbcb9a92b2949211ee82569
Chris Allen <chris.j.allen1979@gmail.com>
2026-03-17 22:58:16 +0000

    Moved files out of conf.d

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,7 +4,7 @@
 
 class orchestrator_cache {
   # HTTP-level cache definition (auto-included by nginx.conf)
-  file { '/etc/puppetlabs/nginx/conf.d/cache_http.conf':
+  file { '/etc/puppetlabs/nginx/includes/cache_http.conf':
     ensure  => file,
     owner   => 'root',
     group   => 'root',
@@ -21,7 +21,7 @@ class orchestrator_cache {
   }
 
   # Location-level snippet
-  file { '/etc/puppetlabs/nginx/conf.d/includes/':
+  file { '/etc/puppetlabs/nginx/includes/':
     ensure => directory,
     owner  => 'root',
     group  => 'root',
@@ -29,23 +29,23 @@ class orchestrator_cache {
   }
 
   # Location-level snippet
-  file { '/etc/puppetlabs/nginx/conf.d/includes/orchestrator_cache.conf':
+  file { '/etc/puppetlabs/nginx/includes/orchestrator_cache.conf':
     ensure  => file,
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
     source  => 'puppet:///modules/orchestrator_cache/orchestrator_cache.conf',
-    require => [File['/etc/puppetlabs/nginx/conf.d/includes/'],File['/etc/puppetlabs/nginx/conf.d/includes/orchestrator_cache_rules.inc'],],
+    require => [File['/etc/puppetlabs/nginx/includes/'],File['/etc/puppetlabs/nginx/includes/orchestrator_cache_rules.inc'],],
   }
 
   # Shared cache rules
-  file { '/etc/puppetlabs/nginx/conf.d/includes/orchestrator_cache_rules.inc':
+  file { '/etc/puppetlabs/nginx/includes/orchestrator_cache_rules.inc':
     ensure  => file,
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
     source  => 'puppet:///modules/orchestrator_cache/orchestrator_cache_rules.inc',
-    require => File['/etc/puppetlabs/nginx/conf.d/includes/'],
+    require => File['/etc/puppetlabs/nginx/includes/'],
   }
 
   # Insert include into PE's server block inside proxy.conf
@@ -53,7 +53,17 @@ class orchestrator_cache {
     directive_ensure => 'present',
     target           => '/etc/puppetlabs/nginx/conf.d/proxy.conf',
     directive_name   => 'include',
-    value            => 'conf.d/includes/orchestrator_cache.conf',
+    value            => 'includes/orchestrator_cache.conf',
     server_context   => $facts['networking']['fqdn'],
   }
+
+
+pe_nginx::directive { 'include cache.conf for pesite2':
+  directive_ensure => 'present',
+  target           => '/etc/puppetlabs/nginx/conf.d/proxy.conf',
+  directive_name   => 'include',
+  value            => 'includes/cache.conf',
+  server_context   => $facts['networking']['fqdn'],
+}
+
 }
