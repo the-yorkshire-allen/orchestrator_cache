8db2ab213626b9973f8b38e195d60b47ed518b1a
Chris Allen <chris.j.allen1979@gmail.com>
2026-03-17 21:13:30 +0000

    Initial release

---
+++ manifests/init.pp
@@ -0,0 +1,28 @@
+class orchestrator_class {
+  # HTTP-level cache definition (auto-included by nginx.conf)
+  file { '/etc/puppetlabs/nginx/conf.d/cache_http.conf':
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => 'puppet:///modules/pe_nginx_cache/cache_http.conf',
+  }
+
+  # Location-level snippet
+  file { '/etc/puppetlabs/nginx/conf.d/orchestrator_cache.conf':
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => 'puppet:///modules/pe_nginx_cache/orchestrator_cache.conf',
+  }
+
+  # Insert include into PE's server block inside proxy.conf
+  pe_nginx::directive { 'include orchestrator cache':
+    directive_ensure => 'present',
+    target           => '/etc/puppetlabs/nginx/conf.d/proxy.conf',
+    directive_name   => 'include',
+    value            => 'orchestrator_cache.conf',
+    server_context   => $facts['networking']['fqdn'],
+  }
+}
